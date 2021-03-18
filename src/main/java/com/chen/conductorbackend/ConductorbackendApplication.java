1b6be7a7993f656270029189fdc6d30a0c76fe91
BillyChen <whqscqm@outlook.com>
2021-03-18 20:22:39 +0800

    first commit

---
+++ src/main/java/com/chen/conductorbackend/ConductorbackendApplication.java
@@ -0,0 +1,13 @@
+package com.chen.conductorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConductorbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConductorbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chen/conductorbackend/ConductorbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.conductorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConductorbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
