9967beb5c5033874547e2fb9bc337c419f658370
BillyChen <whqscqm@outlook.com>
2021-03-23 18:41:57 +0800

    代码生成器生成的结果

--- src/main/java/com/chen/conductorbackend/dto/TaskPostDTO.java
+++ src/main/java/com/chen/conductorbackend/dto/TaskPostDTO.java
@@ -9,20 +9,28 @@ import lombok.Data;
 public class TaskPostDTO {
     @ApiModelProperty(name = "lostName", value = "走失者姓名", dataType = "String", required = true)
     private String lostName;
+
     @ApiModelProperty(name = "lostAge", value = "走失者年龄", dataType = "Integer", required = true)
     private Integer lostAge;
+
     @ApiModelProperty(name = "lostGender", value = "走失者性别", dataType = "String", required = true)
     private String lostGender;
+
     @ApiModelProperty(name = "photo", value = "老人图片url", dataType = "String", required = true)
     private String photo;
+
     @ApiModelProperty(name = "longitude", value = "走失地经度", dataType = "Double", required = true)
     private Integer longitude;
+
     @ApiModelProperty(name = "latitude", value = "走失地纬度", dataType = "Double", required = true)
     private Integer latitude;
+
     @ApiModelProperty(name = "lostAddress", value = "走失者的家庭住址", dataType = "String")
     private String lostAddress;
+
     @ApiModelProperty(name = "lostPhone", value = "走失者家属的联系方式", dataType = "String")
     private String lostPhone;
+
     @ApiModelProperty(name = "detail", value = "走失者的详细信息", dataType = "String")
     private String detail;
 }
---
+++ src/main/java/com/chen/conductorbackend/dto/TaskReturnDTO.java
@@ -0,0 +1,51 @@
+package com.chen.conductorbackend.dto;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+
+public class TaskReturnDTO {
+
+    @ApiModelProperty(value = "救援请求Id")
+    private Integer requestId;
+
+    @ApiModelProperty(value = "走失者姓名")
+    private String lostName;
+
+    @ApiModelProperty(value = "走失者性别")
+    private String lostGender;
+
+    @ApiModelProperty(value = "家属联系方式")
+    private String lostPhone;
+
+    @ApiModelProperty(value = "走失者家庭住址")
+    private String lostAddress;
+
+    @ApiModelProperty(value = "走时者图片url")
+    private String photo;
+
+    @ApiModelProperty(value = "走失时的经度")
+    private Double longitude;
+
+    @ApiModelProperty(value = "走失时的纬度")
+    private Double latitude;
+
+    @ApiModelProperty(value = "详细信息")
+    private String detail;
+
+    @ApiModelProperty(value = "救援状态 0未受理 1进行中 2已完成 3已超时")
+    private String lostStatus;
+
+    @ApiModelProperty(value = "正在救援该老人的人数")
+    private Integer rescueNum;
+
+    private Long gmtCreate;
+
+    @ApiModelProperty(value = "走失者年龄")
+    private Integer lostAge;
+
+
+}
