50ce5c9eaf3a1f0bb8d8d781208f8fb54356cfda
doubleBeans <2018302141022@whu.edu.cn>
2021-03-25 20:53:52 +0800

    管理员登录

--- src/main/java/com/chen/conductorbackend/controller/AdminController.java
+++ src/main/java/com/chen/conductorbackend/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.chen.conductorbackend.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.chen.conductorbackend.common.BaseResult;
 import com.chen.conductorbackend.utils.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +15,37 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AdminController {
 
+    private final String USERNAME = "admin";
+    private final String PASSWORD = "admin";
     @Autowired
     private RedisUtil redisUtil;
 
+    /**
+     * 管理员登录
+     *
+     * @param params 账号和密码
+     * @return code: 0-->success 1-->fail   token
+     */
     @PostMapping("/login")
-    public JSONObject login(@RequestBody JSONObject params) {
-        System.out.println(redisUtil);
-        return null;
+    public Object login(@RequestBody JSONObject params) {
+        String username = params.getObject("username", String.class);
+        String password = params.getObject("password", String.class);
+
+        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            if (redisUtil.hasKey(username)) {
+                redisUtil.expire(username, 24 * 60 * 60);
+            } else {
+                redisUtil.set(username, password, 24 * 60 * 60);
+            }
+
+            log.info("管理员登录成功");
+            JSONObject token = new JSONObject();
+            token.put("token", username);
+            return BaseResult.successWithData(token);
+        } else {
+            log.warn("管理员账号或密码错误");
+            return BaseResult.failWithCodeAndMsg(1, "账号或密码错误");
+        }
     }
 
 }
