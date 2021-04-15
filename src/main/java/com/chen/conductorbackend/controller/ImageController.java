b2bc62b5d219f57ac9d33d2248eaa87154aa7921
BillyChen <whqscqm@outlook.com>
2021-04-15 17:02:41 +0800

    bugFix:文件上传

--- src/main/java/com/chen/conductorbackend/controller/ImageController.java
+++ src/main/java/com/chen/conductorbackend/controller/ImageController.java
@@ -6,10 +6,7 @@ import com.chen.conductorbackend.utils.ImageUtil;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -29,7 +26,7 @@ public class ImageController {
     private ImageUtil imageUtil;
 
     @PostMapping("/upload")
-    public BaseResult imageUpload(MultipartFile upload) {
+    public BaseResult imageUpload(@RequestParam("file") MultipartFile upload) {
         JSONObject jsonObject = new JSONObject();
         try {
             String fileName = upload.getOriginalFilename();
